3551689dfe0bd88e10ee995b3770cb603e542f08
Louis Jasper A. Cañon <132154593+Capsaicin-Enthusiast@users.noreply.github.com>
2025-06-06 15:42:45 +0800

    .

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,10 +1,5 @@
 import { SafeAreaView, TouchableOpacity, View } from 'react-native';
-import {
-  AntDesign,
-  FontAwesome6,
-  Ionicons,
-  MaterialCommunityIcons,
-} from '@expo/vector-icons';
+import { AntDesign, FontAwesome6, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { Slot, useRouter } from 'expo-router';
 
 const TabLayout = () => {
@@ -12,36 +7,37 @@ const TabLayout = () => {
   return (
     <SafeAreaView className="flex-1 bg-white">
       <Slot />
-      <View className="absolute bottom-0 w-full h-18 flex-row justify-around items-center bg-white border-t border-gray-200">
+      <View className="h-18 absolute bottom-0 w-full flex-row items-center justify-around border-gray-200 bg-white">
         <TouchableOpacity
+          className="p-4"
           onPress={() => {
             router.push('/profile');
             console.log('Helloowww');
-          }}
-        >
+          }}>
           <Ionicons name="person" size={24} color="#5A5A5A" />
         </TouchableOpacity>
 
         <TouchableOpacity
+          className="p-4"
           onPress={() => {
             router.push('/');
             console.log('Helloowww');
-          }}
-        >
+          }}>
           <FontAwesome6 name="bars-staggered" size={24} color="#5A5A5A" />
         </TouchableOpacity>
 
-        <AntDesign name="like1" size={24} color="black" />
+        <TouchableOpacity className="p-4">
+          <AntDesign name="like1" size={24} color="black" />
+        </TouchableOpacity>
 
-        <MaterialCommunityIcons
-          name="message"
+        <TouchableOpacity
+          className="p-4"
           onPress={() => {
             console.log('Navigate to messages');
             router.push('/messages');
-          }}
-          size={24}
-          color="#5A5A5A"
-        />
+          }}>
+          <MaterialCommunityIcons name="message" size={24} color="#5A5A5A" />
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
